c7856cfb4420135ed84beccdc174100fa9df880a
tfieldgrass <117985717+tfieldgrass@users.noreply.github.com>
2026-03-31 12:02:42 +0100

    Fix build: use npm install and add base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,8 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://pawdore.com',
+  site: 'https://tfieldgrass.github.io',
+  base: '/pawdore',
   integrations: [sitemap()],
   markdown: {
     shikiConfig: {
